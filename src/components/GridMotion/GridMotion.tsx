95f6c435d560a39871fc2ea348513b928ef209c8
cvazy <player7712@mail.ru>
2025-04-09 02:38:36 +0300

    Изменение стилей карточки товара на главном баннере

--- src/components/GridMotion/GridMotion.tsx
+++ src/components/GridMotion/GridMotion.tsx
@@ -96,7 +96,7 @@ const GridMotion: FC<GridMotionProps> = ({
                 return (
                   <div
                     key={`${rowIndex}-${itemIndex}`}
-                    className="relative aspect-square w-full h-full md:min-w-72 xl:min-w-96"
+                    className="relative w-full h-full md:min-w-72 xl:min-w-96"
                   >
                     <Product />
                   </div>

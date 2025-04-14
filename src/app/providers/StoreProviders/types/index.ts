3a7a1419d6b0fb078d8fff113649b4e93f85a064
cvazy <player7712@mail.ru>
2025-04-15 00:21:56 +0300

    Доработал функционал авторизации/регистрации и логаута. Внедрил RTK для глобального состояния

--- src/app/providers/StoreProviders/types/index.ts
+++
@@ -1,6 +0,0 @@
-import { rootReducer } from "../reducers";
-import { store } from "@/app/providers/StoreProviders/store";
-
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppStore = ReturnType<typeof store>;
-export type AppDispatch = AppStore["dispatch"];
